8f5fa679ab242add6b160670d98117d34ee7cdc5
Chen Dan <danny93me@gmail.com>
2020-08-12 14:29:12 -0400

    update day1

---
+++ 000-first-run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+sudo apt install openbox obconf obmenu tint2 lxterminal lightdm fonts-wqy-microhei lxappearance arandr -yy
+
+sudo apt install geany thunar
+sudo apt install firefox-esr mpv
+# 845
+# geany 2, mousepad 3
+# thunar 96, pcmanfm 114
+
+sudo lightdm
+
+# TODO: color scheme
